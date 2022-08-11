51f6f66b162251e214741e191fe33c697cfc0655
Vadim <vadimir29@gmail.com>
2022-08-11 18:30:33 +0300

    add Layout.jsx and Main.jsx

---
+++ views/Layout.jsx
@@ -0,0 +1,19 @@
+const React = require("react");
+
+const React = ({ children }) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/style/style.css" />
+        <script defer src="/js/index.js"></script>
+        <title>FridayQuiz!</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+};
+
+module.exports = React;
---
+++ views/components/Main.jsx
@@ -0,0 +1,12 @@
+const React = require("react");
+const Layout = require("../Layout");
+
+const Main = () => {
+  return (
+    <Layout>
+      <h1>hello</h1>
+    </Layout>
+  );
+};
+
+module.exports = Main;
