74051acf5c9c0c24ed617dd737242e80141899ad
Vadim <vadimir29@gmail.com>
2022-08-11 21:38:58 +0300

    add checkbox audio

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,5 @@
-console.log(1);
+const checkbox = new Audio("../sound/schelchok.mp3");
+const btnToggle = document.querySelector(".btn-toggle");
+btnToggle.addEventListener("click", () => {
+  checkbox.play();
+});
