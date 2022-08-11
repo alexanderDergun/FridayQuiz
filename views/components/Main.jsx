43b9c62d897274d4691b22b0afc4eff65d543897
Vadim <vadimir29@gmail.com>
2022-08-11 20:26:46 +0300

    add dark theme

--- views/components/Main.jsx
+++ views/components/Main.jsx
@@ -4,13 +4,19 @@ const Layout = require("../Layout");
 const Main = () => {
   return (
     <Layout>
-      <form action="/game" method="POST" name="startGame">
-        <label htmlFor="i1"></label>
-        <input type="text" id="i1" />
-        <button type="submit">enter</button>
-      </form>
-      <div className="test">
-        <button className="btn-toggle">Dark mode</button>
+      <div className="container">
+        <div className="container_title">
+          <h1 className="title">Quiz game</h1>
+        </div>
+        <form action="/game" method="POST" name="startGame">
+          <label htmlFor="i1">Enter your name</label>
+          <input type="text" id="i1" />
+          <button type="submit">enter</button>
+        </form>
+        <div className="test">
+          <input type="checkbox" className="btn-toggle" />
+          <span>Dark mode</span>
+        </div>
       </div>
     </Layout>
   );
