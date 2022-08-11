c4669bbe2bbd2f568dd4023ac255b7bde389dc25
Vika <zhuuvika@gmail.com>
2022-08-11 18:55:33 +0300

    create topic model

---
+++ db/migrations/20220811123905-create-topic.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const attributes = {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      topic: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    };
+
+    await queryInterface.createTable('Topics', attributes);
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Topics');
+  }
+};
