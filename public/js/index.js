1e95d9f41895f8fd53cbfe31b5fd4c9467114e9f
Vadim <vadimir29@gmail.com>
2022-08-12 10:13:08 +0300

    add greeting

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 const checkbox = new Audio("../sound/schelchok.mp3");
 const menu = new Audio("../sound/menu.mp3");
-
+document.querySelector('input[type="checkbox"]').focus();
 const btnToggle = document.querySelector(".btn-toggle");
 btnToggle.addEventListener("click", () => {
   checkbox.play();
@@ -8,7 +8,7 @@ btnToggle.addEventListener("click", () => {
 const topic = document.querySelectorAll(".topic");
 topic.forEach((el) => {
   el.addEventListener("mouseenter", async () => {
-    // await menu.play();
-    console.log(1);
+    await menu.play();
+    // console.log(1);
   });
 });
